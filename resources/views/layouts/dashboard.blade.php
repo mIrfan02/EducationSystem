5f56cdfa777f9f54ea7c7e85eac82161fbebbf9b
Rubab Akmal <rubabakmal19@gmail.com>
2024-06-29 17:25:35 -0400

    admin dashboard styling changed

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,61 +1,140 @@
 @extends('layouts.main')
 
-@section('title','Dashboard')
+@section('title', 'Dashboard')
 
 @section('content')
-<style>
-    .card {
-        border: 1px solid #e6e6e6;
-        border-radius: 10px;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-        overflow: hidden;
-        position: relative;
-        height: 180px;
-    }
+    {{-- <style>
+        .card {
+            border: 1px solid #e6e6e6;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            overflow: hidden;
+            position: relative;
+            height: 180px;
+        }
 
-    .card-title {
-        font-size: 18px;
-        font-weight: bold;
-        text-align: center;
-        margin-top: 60px;
-    }
+        .card-title {
+            font-size: 18px;
+            font-weight: bold;
+            text-align: center;
+            margin-top: 60px;
+        }
 
-    .card-text {
-        font-size: 24px;
-        font-weight: bold;
-        text-align: center;
-    }
+        .card-text {
+            font-size: 24px;
+            font-weight: bold;
+            text-align: center;
+        }
 
-    .grid-margin {
-        margin-bottom: 20px;
-    }
+        .grid-margin {
+            margin-bottom: 20px;
+        }
 
-    .card .overlay {
-        position: absolute;
-        top: 100%;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background: rgba(0, 0, 0, 0.6);
-        color: white;
-        text-align: center;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        transition: top 0.3s ease;
-        font-size: 20px;
-    }
+        .card .overlay {
+            position: absolute;
+            top: 100%;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: rgba(0, 0, 0, 0.6);
+            color: white;
+            text-align: center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            transition: top 0.3s ease;
+            font-size: 20px;
+        }
 
-    .card:hover .overlay {
-        top: 0;
-    }
+        .card:hover .overlay {
+            top: 0;
+        }
 
-    .card a {
-        color: inherit;
-        text-decoration: none;
-    }
-</style>
-<div class="row">
+        .card a {
+            color: inherit;
+            text-decoration: none;
+        }
+    </style> --}}
+    <style>
+        .c-dashboardInfo {
+            margin-bottom: 15px;
+        }
+
+        .c-dashboardInfo .wrap {
+            background: #ffffff;
+            box-shadow: 2px 10px 20px rgba(0, 0, 0, 0.1);
+            border-radius: 7px;
+            text-align: center;
+            position: relative;
+            overflow: hidden;
+            padding: 40px 25px 20px;
+            height: 100%;
+        }
+
+        .c-dashboardInfo__title,
+        .c-dashboardInfo__subInfo {
+            color: #6c6c6c;
+            font-size: 1.18em;
+        }
+
+        .c-dashboardInfo span {
+            display: block;
+        }
+
+        .c-dashboardInfo__count {
+            font-weight: 600;
+            font-size: 2.5em;
+            line-height: 64px;
+            color: #323c43;
+        }
+
+        .c-dashboardInfo .wrap:after {
+            display: block;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 10px;
+            content: "";
+        }
+
+        .c-dashboardInfo:nth-child(1) .wrap:after {
+            background: linear-gradient(82.59deg, #00c48c 0%, #00a173 100%);
+        }
+
+        .c-dashboardInfo:nth-child(2) .wrap:after {
+            background: linear-gradient(81.67deg, #0084f4 0%, #1a4da2 100%);
+        }
+
+        .c-dashboardInfo:nth-child(3) .wrap:after {
+            background: linear-gradient(69.83deg, #0084f4 0%, #00c48c 100%);
+        }
+
+        .c-dashboardInfo:nth-child(4) .wrap:after {
+            background: linear-gradient(81.67deg, #ff647c 0%, #1f5dc5 100%);
+        }
+
+        .c-dashboardInfo__title svg {
+            color: #d7d7d7;
+            margin-left: 5px;
+        }
+
+        .MuiSvgIcon-root-19 {
+            fill: currentColor;
+            width: 1em;
+            height: 1em;
+            display: inline-block;
+            font-size: 24px;
+            transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+            user-select: none;
+            flex-shrink: 0;
+        }
+
+        a {
+            text-decoration: none
+        }
+    </style>
+    {{-- <div class="row">
     <h2>Overview</h2>
     <div class="col-md-4 grid-margin stretch-card">
         <div class="card bg-primary text-white">
@@ -87,5 +166,61 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
+
+
+    <div id="root">
+        <div class="container pt-5">
+            <div class="row align-items-stretch">
+                <div class="c-dashboardInfo col-lg-4 col-md-6">
+                    <a href="{{ route('categories') }}">
+
+                        <div class="wrap">
+                            <h4 class="heading heading5 hind-font medium-font-weight c-dashboardInfo__title">
+                                Categories<svg class="MuiSvgIcon-root-19" focusable="false" viewBox="0 0 24 24"
+                                    aria-hidden="true" role="presentation">
+                                    <path fill="none" d="M0 0h24v24H0z"></path>
+                                    <path
+                                        d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z">
+                                    </path>
+                                </svg></h4><span
+                                class="hind-font caption-12 c-dashboardInfo__count">{{ $categoryCount }}</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="c-dashboardInfo col-lg-4 col-md-6">
+                    <div class="wrap">
+                        <a href="{{ route('courses.index') }}">
+                            <h4 class="heading heading5 hind-font medium-font-weight c-dashboardInfo__title"> Courses<svg
+                                    class="MuiSvgIcon-root-19" focusable="false" viewBox="0 0 24 24" aria-hidden="true"
+                                    role="presentation">
+                                    <path fill="none" d="M0 0h24v24H0z"></path>
+                                    <path
+                                        d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z">
+                                    </path>
+                                </svg></h4><span
+                                class="hind-font caption-12 c-dashboardInfo__count">{{ $courseCount }}</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="c-dashboardInfo col-lg-4 col-md-6">
+                    <div class="wrap">
+                        <a href="{{ route('teachers.index') }}">
+
+                            <h4 class="heading heading5 hind-font medium-font-weight c-dashboardInfo__title">Teachers<svg
+                                    class="MuiSvgIcon-root-19" focusable="false" viewBox="0 0 24 24" aria-hidden="true"
+                                    role="presentation">
+                                    <path fill="none" d="M0 0h24v24H0z"></path>
+                                    <path
+                                        d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z">
+                                    </path>
+                                </svg></h4><span
+                                class="hind-font caption-12 c-dashboardInfo__count">{{ $teacherCount }}</span>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/studentdashbaord.blade.php
+++ resources/views/layouts/studentdashbaord.blade.php
@@ -1,85 +1,90 @@
 @extends('layouts.main')
 
-@section('title','Dashboard')
+@section('title', 'Dashboard')
 
 @section('content')
-<style>
-    .card {
-        border: 1px solid #e6e6e6;
-        border-radius: 10px;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-        position: relative;
-        overflow: hidden;
-        background-color: #f8f9fa;
-        transition: transform 0.3s, box-shadow 0.3s;
-        height: 250px; /* Increased height */
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+    <style>
+        .c-dashboardInfo {
+            margin-bottom: 15px;
+        }
 
-    .card:hover {
-        transform: translateY(-10px);
-        box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
-    }
+        .c-dashboardInfo .wrap {
+            background: #ffffff;
+            box-shadow: 2px 10px 20px rgba(0, 0, 0, 0.1);
+            border-radius: 7px;
+            text-align: center;
+            position: relative;
+            overflow: hidden;
+            padding: 40px 25px 20px;
+            height: 100%;
+        }
 
-    .card-title {
-        font-size: 18px;
-        font-weight: bold;
-        color: #ffffff;
-    }
+        .c-dashboardInfo__title,
+        .c-dashboardInfo__subInfo {
+            color: #6c6c6c;
+            font-size: 1.18em;
+        }
 
-    .card-text {
-        font-size: 24px;
-        font-weight: bold;
-        color: #ffffff;
-        display: none;
-    }
+        .c-dashboardInfo span {
+            display: block;
+        }
 
-    .card-hover-text {
-        display: none;
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, 0.7);
-        color: #ffffff;
-        align-items: center;
-        justify-content: center;
-        font-size: 24px;
-        font-weight: bold;
-        text-align: center;
-        transition: opacity 0.3s;
-        opacity: 0;
-    }
+        .c-dashboardInfo__count {
+            font-weight: 600;
+            font-size: 2.5em;
+            line-height: 64px;
+            color: #323c43;
+        }
 
-    .card:hover .card-hover-text {
-        display: flex;
-        opacity: 1;
-    }
+        .c-dashboardInfo .wrap:after {
+            display: block;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 10px;
+            content: "";
+        }
 
-    .grid-margin {
-        margin-bottom: 20px;
-    }
+        .c-dashboardInfo:nth-child(1) .wrap:after {
+            background: linear-gradient(82.59deg, #00c48c 0%, #00a173 100%);
+        }
 
-    .bg-sessions {
-        background-color: #6f42c1; /* Purple */
-    }
+        .c-dashboardInfo:nth-child(2) .wrap:after {
+            background: linear-gradient(81.67deg, #0084f4 0%, #1a4da2 100%);
+        }
 
-    .bg-bookings {
-        background-color: #007bff; /* Blue */
-    }
-    .bg-new {
-        background-color: #3c98aa; /* Blue */
-    }
+        .c-dashboardInfo:nth-child(3) .wrap:after {
+            background: linear-gradient(69.83deg, #0084f4 0%, #00c48c 100%);
+        }
 
-    .bg-wallet {
-        background-color: #28a745; /* Green */
-    }
-</style>
+        .c-dashboardInfo:nth-child(4) .wrap:after {
+            background: linear-gradient(81.67deg, #ff647c 0%, #1f5dc5 100%);
+        }
 
-<div class="row">
+        .c-dashboardInfo__title svg {
+            color: #d7d7d7;
+            margin-left: 5px;
+        }
+
+        .MuiSvgIcon-root-19 {
+            fill: currentColor;
+            width: 1em;
+            height: 1em;
+            display: inline-block;
+            font-size: 24px;
+            transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+            user-select: none;
+            flex-shrink: 0;
+        }
+
+        a {
+            text-decoration: none;
+        }
+    </style>
+
+
+    {{-- <div class="row">
     <h2>Summary</h2>
 
     <div class="col-md-4 grid-margin stretch-card">
@@ -100,5 +105,43 @@
             </div>
         </a>
     </div>
-</div>
+</div> --}}
+    <div id="root">
+        <div class="container pt-5">
+            <div class="row align-items-stretch">
+                <div class="c-dashboardInfo col-lg-6 col-md-6">
+                    <a href="{{ route('student.bookings') }}">
+
+                        <div class="wrap">
+                            <h4 class="heading heading5 hind-font medium-font-weight c-dashboardInfo__title">
+                                My Bookings<svg class="MuiSvgIcon-root-19" focusable="false" viewBox="0 0 24 24"
+                                    aria-hidden="true" role="presentation">
+                                    <path fill="none" d="M0 0h24v24H0z"></path>
+                                    <path
+                                        d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z">
+                                    </path>
+                                </svg></h4><span
+                                class="hind-font caption-12 c-dashboardInfo__count">{{ $bookingCount }}</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="c-dashboardInfo col-lg-6 col-md-6">
+                    <div class="wrap">
+                        <a href="{{ route('welcome.index') }}">
+                            <h4 class="heading heading5 hind-font medium-font-weight c-dashboardInfo__title">
+                                New Bookings<svg class="MuiSvgIcon-root-19" focusable="false" viewBox="0 0 24 24"
+                                    aria-hidden="true" role="presentation">
+                                    <path fill="none" d="M0 0h24v24H0z"></path>
+                                    <path
+                                        d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z">
+                                    </path>
+                                </svg></h4><span class="hind-font caption-12 c-dashboardInfo__count">+</span>
+                        </a>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
 @endsection
